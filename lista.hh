d06b1fa0025a92695c18460e2332400e00903be5
lglowack <poiqwe97@gmail.com>
2019-04-27 18:30:56 +0200

    Dodana lista 2 kierunkowa

--- lista.hh
+++ lista.hh
@@ -1,16 +1,162 @@
 #include <iostream>
-
-template<typename Typ>
+using namespace std;
+template <typename Typ>
 class Element
 {
-   Typ T;
-   *Element Nastepny;
-   *Element Poprzedni;
+
+    Typ Zawartosc;
+    Element<Typ> *Nastepny;
+    Element<Typ> *Poprzedni;
+    ///////////////////////////////////
+  public:
+    Element<Typ> *Get_Nastepny()
+    {
+        return Nastepny;
+    }
+    Element<Typ> *Get_Poprzedni()
+    {
+        return Poprzedni;
+    }
+    Typ &Get_Zawartosc()
+    {
+        return Zawartosc;
+    }
+    void Set_Zawartosc(Typ &zawar)
+    {
+        Zawartosc = zawar;
+    }
+    void Set_Nastepmny(Element<Typ> *wsk)
+    {
+        Nastepny = wsk;
+    }
+    void Set_Poprzedni(Element<Typ> *wsk)
+    {
+        Poprzedni = wsk;
+    }
 };
-
-
-template <typename typ>
-class Lista 
+/////////////////////////////////////////////////////////////////
+/*      Lista           */
+/////////////////////////////////////////////////////////////////
+template <typename Typ>
+class Lista
 {
-    *Element<typ> Start;
+    Element<Typ> *Start;
+    int Ile;
+
+  public:
+    Lista();
+    ~Lista();
+    void Dodaj_Element(Typ T, int nr);
+    Typ Usun_Element(int nr);
+    void Show();
 };
+
+/*  Konstruktor */
+template <typename Typ>
+Lista<Typ>::Lista()
+{
+    Start = NULL;
+    Ile = 0;
+}
+
+/*  Destruktor  */
+template <typename Typ>
+Lista<Typ>::~Lista()
+{
+}
+
+/*  Dodawanie elementów  */
+template <typename Typ>
+void Lista<Typ>::Dodaj_Element(Typ T, int nr)
+{
+    Element<Typ> *nowy_el = new Element<Typ>();
+    nowy_el->Set_Zawartosc(T);
+    nowy_el->Set_Poprzedni(NULL);
+    nowy_el->Set_Nastepmny(Start);
+    if (Start != NULL)
+    {
+        Start->Set_Poprzedni(nowy_el);
+    }
+    Start = nowy_el;
+    ++Ile;
+}
+
+/* Pokazanie zawartosci listy */
+template <typename Typ>
+void Lista<Typ>::Show()
+{
+    Element<Typ> *el = Start;
+    while (el != NULL)
+    {
+        cout << " " << el->Get_Zawartosc();
+        el = el->Get_Nastepny();
+    }
+}
+/* Usun Element */
+template <typename Typ>
+Typ Lista<Typ>::Usun_Element(int nr)
+{
+    Element<Typ> *el = Start;
+    Element<Typ> *el_poprzedni;
+    Element<Typ> *el_nastepny;
+    Typ T;
+    if (nr == 0)
+    {
+        if(el!=NULL)
+        {
+           if (el->Get_Nastepny() == NULL) ///Sprawdzam czy nastepny element nie jest ostanim
+            {
+                Start=NULL;
+                T = el->Get_Zawartosc();
+                delete el;
+            }
+            else
+            {
+                  Start = el->Get_Nastepny();
+                  Start->Set_Poprzedni(NULL);
+                  T = el->Get_Zawartosc();
+                delete el;
+            } 
+            --Ile;
+        }
+        else
+        {
+            cout<<"Lista pusta!!";
+        }
+    }
+    else
+    {
+
+        if (nr >= Ile)
+        {
+            cout << " za duzy nr nie ma tyle el \n";
+        }
+        else
+        {
+            for (int i = nr; i > 0; --i)
+            {
+                el = el->Get_Nastepny();
+            }
+            if (el->Get_Nastepny() == NULL) ///Sprawdzam czy nastepny element nie jest ostanim
+            {
+                el_poprzedni = el->Get_Poprzedni();
+                el_poprzedni->Set_Nastepmny(NULL);
+                T = el->Get_Zawartosc();
+                delete el;
+            }
+            else
+            {
+                el_poprzedni = el->Get_Poprzedni();
+                el_nastepny = el->Get_Nastepny();
+
+                el_nastepny->Set_Poprzedni(el_poprzedni);
+                el_poprzedni->Set_Nastepmny(el_nastepny);
+
+                T = el->Get_Zawartosc();
+                delete el;
+            }
+            --Ile;
+        }
+    }
+    return T;
+}
--- main.cpp
+++ main.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
-
+#include "lista.hh"
 #include "graf.hh"
 
 int main()
 {
-    for (int j = 7; j < 10; j++)
-    {
-        Graf G(j);
-        for (int i = 0; i < 4; i++)
-        {
-            G.Zeruj();
-            G.Stworz_Graf(i);
+    // for (int j = 7; j < 10; j++)
+    // {
+    //     Graf G(j);
+    //     for (int i = 0; i < 4; i++)
+    //     {
+    //         G.Zeruj();
+    //         G.Stworz_Graf(i);
 
-            cout << G;
-        }
+    //         cout << G;
+    //     }
+    // }
+    Lista<int> a;
+    for (int i = 0; i < 10; ++i)
+    {
+        a.Dodaj_Element(i, 0);
+        a.Show();
+        cout<<endl;
+    }
+     for (int i = 0; i < 45; ++i)
+    {
+        a.Usun_Element(0);
+        a.Show();
+        cout<<endl;
     }
     return 0;
 }
